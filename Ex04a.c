d44cddd18daa2ebd57f637ead561fa219068f86b
Taiki Minakawa <s1290041@u-aizu.ac.jp>
2022-07-14 19:00:55 +0900

    HoT: initial revision

---
+++ Ex04a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int i,h=0,t=0,A[5];
+  unsigned int seed;
+  printf("Tossing a coins\n");
+    seed = (unsigned int)time(NULL);
+    srand(seed);
+   for(i = 1;i < 4;i++){
+     A[i] = rand()%2;
+    if(A[i] == 0){
+      printf("Heads\n");
+      h++;
+    }
+    else{
+      printf("Tails\n");
+      t++;
+    }
+  }
+    printf("Heads %d Tails %d\n",h,t);
+   return 0;
+}
